faa1cf0db07745fbbb9adec58c92bfe7eb64ba9e
ZAYN MOHAMMED <zaynmohammed@ZAYNs-MacBook-Air.local>
2023-10-27 10:42:25 +0530

    first sucessful live deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthenticationComponent } from './authentication/authentication.component';
-import { UserService } from './user.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AuthenticationComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(UserService)
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
